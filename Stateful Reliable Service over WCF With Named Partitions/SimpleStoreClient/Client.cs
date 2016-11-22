f82ae2dc73a69aa18cdfefbbc97ab2c7e0673772
Simon Hart <simon@simonrhart.com>
2016-11-22 18:26:44 +0100

    added stateful named partition sample

---
+++ Stateful Reliable Service over WCF With Named Partitions/SimpleStoreClient/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace SimpleStoreClient
+{
+    public class Client : ServicePartitionClient<WcfCommunicationClient<IShoppingCartService>>, IShoppingCartService
+    {
+        public Client(WcfCommunicationClientFactory<IShoppingCartService> clientFactory, Uri serviceName, ServicePartitionKey customerId)
+                : base(clientFactory, serviceName, customerId)
+        {
+        }
+
+        public Task AddItem(ShoppingCartItem item)
+        {
+            return this.InvokeWithRetryAsync(client => client.Channel.AddItem(item));
+        }
+
+        //public Task DeleteItem(string productName)
+        //{
+        //    return this.InvokeWithRetryAsync(client => client.Channel.DeleteItem(productName));
+        //}
+
+        public Task<IList<KeyValuePair<string, ShoppingCartItem>>> GetItems()
+        {
+            return this.InvokeWithRetryAsync(client => client.Channel.GetItems());
+        }
+    }
+}
---
+++ Stateful Reliable Service over WCF With Named Partitions/SimpleStoreClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace SimpleStoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri ServiceName = new Uri("fabric:/SimpleStoreApplication/ShoppingCartService");
+
+            ServicePartitionResolver serviceResolver = new ServicePartitionResolver(() => new FabricClient());
+            NetTcpBinding binding = CreateClientConnectionBinding();
+            ServicePartitionKey partitionKey = new ServicePartitionKey(0);
+                Client shoppingClient =
+                 new Client(
+                     new WcfCommunicationClientFactory<IShoppingCartService>(servicePartitionResolver: serviceResolver, clientBinding: binding),
+                     ServiceName, partitionKey);
+
+            //shoppingClient.AddItem(new ShoppingCartItem
+            //{
+            //    ProductName = "XBOX ONE",
+            //    UnitPrice = 329.0,
+            //    Amount = 1
+            //}).Wait();
+            PrintPartition(shoppingClient);
+           
+            var list = shoppingClient.GetItems().Result;
+          
+           
+        }
+        private static NetTcpBinding CreateClientConnectionBinding()
+        {
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None)
+            {
+                SendTimeout = TimeSpan.MaxValue,
+                ReceiveTimeout = TimeSpan.MaxValue,
+                OpenTimeout = TimeSpan.FromSeconds(5),
+                CloseTimeout = TimeSpan.FromSeconds(5),
+                MaxReceivedMessageSize = 1024 * 1024
+            };
+            binding.MaxBufferSize = (int)binding.MaxReceivedMessageSize;
+            binding.MaxBufferPoolSize = Environment.ProcessorCount * binding.MaxReceivedMessageSize;
+
+            return binding;
+        }
+        private static void PrintPartition(Client client)
+        {
+            ResolvedServicePartition partition;
+            if (client.TryGetLastResolvedServicePartition(out partition))
+            {
+                Console.WriteLine("Partition ID: " + partition.Info.Id);
+            }
+        }
+    }
+}
